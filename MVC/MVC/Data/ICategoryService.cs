5e39f26c8a0485a495e88284924e5b16d6328cab
Fz40 <ftet1sds@gmail.com>
2021-01-11 18:11:49 +0700

    All New

---
+++ MVC/MVC/Data/ICategoryService.cs
@@ -0,0 +1,29 @@
+﻿
+using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC.Data
+{
+    public interface ICategoryService
+    {
+
+        IEnumerable<CategoryModel> GetAllCategoty();
+
+        Task<IEnumerable<CategoryModel>> GetAll_Categoty();
+
+        Task<CategoryModel> GetCategoryById(int? id);
+
+        Task<IEnumerable<CategoryModel>> GetCategoryByCondition(CategoryModel cat);
+
+        Task<string> CreateCategory(CategoryModel cat);
+
+        Task<string> UpdateCategory(CategoryModel cat);
+
+        Task<string> DeleteCategory(int? id);
+        
+    }
+}
---
+++ MVC/MVC/Models/ConditionModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class ConditionModel
+    {
+        public string encrypt { get; set; }
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public void clear ()
+        {
+            this.encrypt = "";
+            this.id = 0;
+            this.Name = "";
+            this.Description = "";
+        }
+
+    }
+}
